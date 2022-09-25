b5655decbdc7e9b107cc34b25c2b0f3d7c5f181b
WrightRocket <keith.wright.rocket@gmail.com>
2022-09-24 21:11:39 -0700

    Working inline patio module

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
-    println!("Hello, welcome to my balcony!");
 
     mod patio { // Inline module declaration
-        enum Furniture {
-            Chair,
-            Table,
-            Umbrella,
+        #[derive(Debug)]
+        pub enum Furniture {
+            Chair(String),
+            Rug(String),
+            Table(String),
+            Umbrella(String),
         }
-    } 
 
-    
+    }
+
     // mod flowers; // Uses file path src/flowers.rs
 
     // mod herbs; // Uses file path src/herbs/mod.rs
+    
+    println!("Hello, welcome to my balcony!");
+    let chairs = patio::Furniture::Chair(String::from("Two teal chairs."));
+    dbg!(chairs); 
+    let mat = patio::Furniture::Rug(String::from("Modrian welcome mat."));
+    println!("Outside of the sliding door is a {:?}.", mat);
+    let glass = patio::Furniture::Table(String::from("Rectangular patio table."));
+    println!("Where most of the herbs are located: {:#?}", glass);
+    let vinyl = patio::Furniture::Umbrella(String::from("Tan nine foot crankable umbrella."));
+    println!("What keeps the balcony cool? {:#?}", vinyl);
+
 }
